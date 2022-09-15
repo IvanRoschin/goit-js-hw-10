948eade36c62046b13b0958d7de5601fb19953ce
Ivan Roschin <ivan.roschin86@gmail.com>
2022-09-15 18:11:26 +0300

    update error

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,14 +1,9 @@
-import Notiflix from 'notiflix';
-
 const BASE_URL = 'https://restcountries.com/v2/';
 
 function fetchCountries(countryName) {
   return fetch(
     `${BASE_URL}name/${countryName}?fields=name,capital,population,flags,languages`
   ).then(response => {
-    if (response.status !== 200) {
-      Notiflix.Notify.failure(`❌ Fetc не сработал, ошибка API сервера`);
-    }
     return response.json();
   });
 }
