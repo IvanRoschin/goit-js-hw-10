568c9dcde9afc0efbdc800937147bd2e3ff0c7a0
Ivan Roschin <ivan.roschin86@gmail.com>
2022-09-15 04:09:54 +0300

    all add

---
+++ src/js/get-refs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+  return {
+    countryListCardContainer: document.querySelector('.country-list'),
+    countryInfoCardContainer: document.querySelector('.country-info'),
+    searchForm: document.querySelector('#search-box'),
+  };
+}
